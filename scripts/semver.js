1e4ad5104ec596e4139a544d4b3c4804ab94a1da
ItsRauf <raufislam2003@gmail.com>
2021-05-17 12:50:24 -0400

    ci(main): add semver incrementing

---
+++ scripts/semver.js
@@ -0,0 +1,24 @@
+import { readFile, writeFile } from 'fs/promises';
+import semver from 'semver';
+
+(async () => {
+  const packageJSON = await readFile('./package.json');
+  const { name, version, ...pkg } = JSON.parse(packageJSON);
+  const commits = process.env.GitHubCommits
+    ? JSON.parse(process.env.GitHubCommits)
+    : [];
+  let rel = 'patch';
+  if (commits.length <= 1) {
+    rel = 'patch';
+  } else if (commits.length <= 10) {
+    rel = 'minor';
+  } else {
+    rel = 'major';
+  }
+  const semverVersion = semver.inc(version, rel);
+  await writeFile(
+    './package.json',
+    JSON.stringify({ name, version: semverVersion, ...pkg }, null, 2)
+  );
+  console.log(semverVersion);
+})();
