10bf6852406169204848a3f365bb3615ad75fa2d
ItsRauf <raufislam2003@gmail.com>
2021-05-18 14:09:58 -0400

    feat(User): create dm method

--- src/classes/User.ts
+++ src/classes/User.ts
@@ -1,7 +1,33 @@
+import { RESTPostAPICurrentUserCreateDMChannelResult } from 'discord-api-types/v8';
 import { BaseUser } from './BaseUser';
+import { DMChannel } from './DMChannel';
 
 export class User extends BaseUser {
   async edit(): Promise<void> {
     return Promise.reject(new Error('Edit not allowed on this Class'));
+  }
+
+  /**
+   * Creates a DM with the current user
+   * {@link https://discord.com/developers/docs/resources/user#create-dm}
+   *
+   * ---
+   * @param {boolean} [cached=true]
+   * @return {*}  {Promise<DMChannel>}
+   * @memberof User
+   */
+  async createDM(cached = true): Promise<DMChannel> {
+    try {
+      const existing = this.$.directMessages
+        .array()
+        .find(channel => channel.recipients?.includes(this));
+      if (existing && cached) {
+        return existing;
+      } else {
+        return this.$.directMessages.fetch(this.id);
+      }
+    } catch (error) {
+      return Promise.reject(error);
+    }
   }
 }
